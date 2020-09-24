338580ffe46dec3d407d0924c5208b3e75c69211
elliottnes <60632551+elliottnes@users.noreply.github.com>
2020-09-24 08:24:04 -0700

    Initial Batch - first upload of homepg and muralpg

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+/* Open */
+function openNav1() {
+  document.getElementById("myNav1").style.height = "100%"; 
+}
+
+/* Close */
+function closeNav1() {
+  document.getElementById("myNav1").style.height = "0%";
+}
